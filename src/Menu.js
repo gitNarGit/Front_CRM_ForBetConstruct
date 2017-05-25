61bee79d0c35d06d0dfe05e31fbc251ff04a7d64
argishtiyegh <eagis92@gmail.com>
2017-05-26 03:34:56 +0400

    Files added up to date

---
+++ src/Menu.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import './StyleSheet/Menu.css';
+
+class Menu extends Component{
+  render(){
+    return(
+      
+          <div className="menu">
+                  <ul className="list_menu">
+                      <li className="menu_item"><NavLink activeClassName="active" to='/table'>Table </NavLink></li>
+                      <li className="menu_item"><NavLink activeClassName="active" to='/addmailinglist'>MailList </NavLink></li>
+                      <li className="menu_item"><NavLink activeClassName="active" to='/sendemails'> Send Email </NavLink></li>
+                 </ul>
+          </div>
+      
+    );
+  }
+}
+
+export default Menu;
